dadedaabef8843fca197ad066fd3c7166ac80b57
laurentiu-piciu <laurentiupiciu@gmail.com>
2018-04-02 22:45:21 +0300

    project initialization

---
+++ app/src/main/java/ro/pub/cs/systems/eim/demo_test_eim/PractivalTest01MainActivity.java
@@ -0,0 +1,13 @@
+package ro.pub.cs.systems.eim.demo_test_eim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PractivalTest01MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practival_test01_main);
+    }
+}
